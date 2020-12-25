e5e8ba7e2bedef9ab5623a269c56d839b1af588f
Alexei Tighineanu <alexei.tighineanu@gmail.com>
2020-12-25 13:54:15 +0100

    fixed workdir for ssh key

--- pkg/utils/ssh.go
+++ pkg/utils/ssh.go
@@ -1,10 +1,20 @@
 package utils
 
-"os/exec"
+import (
+	"os/exec"
+	"os"
+	"path/filepath"
+	"fmt"
+)
+
 
 func SSHCommand(IP string, cmd ...string) *exec.Cmd {
+	workdir, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("pkg/utils/ssh.go ERROR: %v\n", err)
+	}
 	arg := append(
-		[]string{"-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile /dev/null", "-i", filepath.Join(os.Getwd(), "ssh/id_rsa"),
+		[]string{"-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile /dev/null", "-i", filepath.Join(workdir, "ssh/id_rsa"),
 			fmt.Sprintf("root@%s", IP),
 		},
 		cmd...,
