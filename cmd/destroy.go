37af21036bba20bc44552ac3c66e1a3be23abc50
atighineanu <atighineanu@suse.de>
2021-04-12 15:02:43 +0200

    fixed command description

--- cmd/destroy.go
+++ cmd/destroy.go
@@ -20,7 +20,6 @@ import (
 	"clusterer/pkg/utils"
 	"log"
 	"path/filepath"
-
 	"github.com/spf13/cobra"
 )
 
@@ -28,13 +27,8 @@ import (
 var (
 	destroyCmd = &cobra.Command{
 		Use:   "destroy",
-		Short: "A brief description of your command",
-		Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+		Short: "destroys all the machines in the cluster.",
+		Long: ``,
 		Run: func(cmd *cobra.Command, args []string) {
 			Run()
 		},
